1b0f8fca196131aa3c22208d7946732f72dd96f1
guminskaya484@gmail.com <guminskaya484@gmail.com>
2024-10-30 12:33:24 +0300

    Добавьте файлы проекта.

---
+++ HangmanGame/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace HangmanGame
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Элементы управления
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblAttempts;
+        private System.Windows.Forms.TextBox txtLetter;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.ListBox lstGuessedLetters;
+        private System.Windows.Forms.Button btnNewGame;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.txtLetter = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lstGuessedLetters = new System.Windows.Forms.ListBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // lblWord
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(30, 30);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(150, 29);
+            this.lblWord.Text = "Слово: ______";
+
+            // lblAttempts
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Location = new System.Drawing.Point(30, 80);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(120, 13);
+            this.lblAttempts.Text = "Осталось попыток: 6";
+
+            // txtLetter
+            this.txtLetter.Location = new System.Drawing.Point(30, 120);
+            this.txtLetter.Name = "txtLetter";
+            this.txtLetter.Size = new System.Drawing.Size(100, 20);
+
+            // btnGuess
+            this.btnGuess.Location = new System.Drawing.Point(150, 120);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.Text = "Угадать";
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+
+            // lstGuessedLetters
+            this.lstGuessedLetters.FormattingEnabled = true;
+            this.lstGuessedLetters.Location = new System.Drawing.Point(30, 160);
+            this.lstGuessedLetters.Name = "lstGuessedLetters";
+            this.lstGuessedLetters.Size = new System.Drawing.Size(120, 95);
+
+            // btnNewGame
+            this.btnNewGame.Location = new System.Drawing.Point(150, 160);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.Text = "Новая игра";
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(284, 281);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.txtLetter);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lstGuessedLetters);
+            this.Controls.Add(this.btnNewGame);
+            this.Name = "Form1";
+            this.Text = "Виселица";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ HangmanGame/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    public partial class Form1 : Form
+    {
+        private string wordToGuess;
+        private string maskedWord;
+        private int attemptsLeft;
+        private HashSet<char> guessedLetters = new HashSet<char>();
+        private List<string> wordList = new List<string> { "КОМПЬЮТЕР", "ПРОГРАММА", "ЯЗЫК", "ПО", "РАЗРАБОТЧИК", "УЧЕБА", "РЕБЕНОК", "ПРАВИЛА", "МАШИНА", "ПРОГРАММИСТ" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            // Выбираем случайное слово из списка
+            Random random = new Random();
+            wordToGuess = wordList[random.Next(wordList.Count)];
+            attemptsLeft = 6;
+            guessedLetters.Clear();
+            maskedWord = new string('_', wordToGuess.Length);
+
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            // Обновляем отображение слова и оставшихся попыток
+            lblWord.Text = maskedWord;
+            lblAttempts.Text = $"Осталось попыток: {attemptsLeft}";
+            lstGuessedLetters.Items.Clear();
+            foreach (var letter in guessedLetters)
+            {
+                lstGuessedLetters.Items.Add(letter);
+            }
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (txtLetter.Text.Length == 1)
+            {
+                char guessedLetter = char.ToUpper(txtLetter.Text[0]);
+                txtLetter.Clear();
+
+                if (!guessedLetters.Contains(guessedLetter))
+                {
+                    guessedLetters.Add(guessedLetter);
+
+                    if (wordToGuess.Contains(guessedLetter.ToString()))
+                    {
+                        // Открываем буквы в слове
+                        char[] maskedArray = maskedWord.ToCharArray();
+                        for (int i = 0; i < wordToGuess.Length; i++)
+                        {
+                            if (wordToGuess[i] == guessedLetter)
+                            {
+                                maskedArray[i] = guessedLetter;
+                            }
+                        }
+                        maskedWord = new string(maskedArray);
+
+                        // Проверка на победу
+                        if (!maskedWord.Contains('_'))
+                        {
+                            MessageBox.Show("Поздравляем! Вы угадали слово!");
+                            StartNewGame();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        attemptsLeft--;
+
+                        // Проверка на проигрыш
+                        if (attemptsLeft <= 0)
+                        {
+                            MessageBox.Show($"Вы проиграли! Загаданное слово было: {wordToGuess}");
+                            StartNewGame();
+                            return;
+                        }
+                    }
+                }
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Введите одну букву.");
+            }
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
